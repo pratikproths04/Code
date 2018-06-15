9837b0f65caa1995ebd02ed874c9836d79f032f3
Zhiyi Chen <zhiyi.chen83@gmail.com>
2018-06-14 20:11:46 -0700

    update 6/14

--- Time_Space_Complexity.java
+++ Time_Space_Complexity.java
@@ -3283,6 +3283,126 @@ Practice Q9:
     }
 
 
+143.
+	//find middle --> reverse --> reorder
+	public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode mid = findMid(head);
+        mid = reverse(mid);
+        ListNode pointer;
+        while (mid != null) {
+            pointer = head.next;
+            head.next = new ListNode(mid.val);
+            head.next.next = pointer;
+            mid = mid.next;
+            head = head.next.next;
+        }
+        //must do on the list before
+        //for void output
+    }
+    
+    private ListNode findMid(ListNode head) {
+        int count = 0;
+        ListNode pointer = head;
+        while (pointer != null) {
+            pointer = pointer.next;
+            count ++;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        for (int i = 0; i < count - count / 2; i ++) {
+            dummy = dummy.next;
+        }
+        head = dummy.next;
+        dummy.next = null;
+        return head;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode pointer = head;
+        while (pointer != null) {
+            head = head.next;
+            pointer.next = dummy.next;
+            dummy.next = pointer;
+            pointer = head;
+        }
+        return dummy.next;
+    }
+
+    //another method
+    public void reorderList(ListNode head) {
+        if(head==null||head.next==null) return;
+        
+        //Find the middle of the list
+        ListNode p1=head;
+        ListNode p2=head;
+        while(p2.next!=null&&p2.next.next!=null){ 
+            p1=p1.next;
+            p2=p2.next.next;
+        }
+        
+        //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
+        ListNode preMiddle=p1;
+        ListNode preCurrent=p1.next;
+        while(preCurrent.next!=null){
+            ListNode current=preCurrent.next;
+            preCurrent.next=current.next;
+            current.next=preMiddle.next;
+            preMiddle.next=current;
+        }
+        
+        //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
+        p1=head;
+        p2=preMiddle.next;
+        while(p1!=preMiddle){
+            preMiddle.next=p2.next;
+            p2.next=p1.next;
+            p1.next=p2;
+            p1=p2.next;
+            p2=preMiddle.next;
+        }
+    }
+
+
+445.
+	//using stacks
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+        
+        return list.val == 0 ? list.next : list;
+    }
+
+
+
+
+
+
+
+
+
 
 
 
