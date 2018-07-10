a95f377a4890308af2ba43c63b40a03e97aec393
Zhiyi Chen <zhiyi.chen83@gmail.com>
2018-07-09 20:29:12 -0700

    update

--- crack_the_interview.java
+++ crack_the_interview.java
@@ -1,4 +1,4 @@
-Behind the Scenes:
+Behind the Scenes: {
 	screening interview --> interview in-person
 	oneline synchronized document editors
 	write code on paper and read it back over the phone
@@ -24,34 +24,229 @@ Behind the Scenes:
 	be respectfil when you do that
 	They are busy and forgetful
 
-Microsoft Interview:
-	want passionate people about technology and smart people
-	write code on the board
-	1.	show up at Microsoft and fill out initial paper work
-	2.	short interview and a sample question, prep you
-	3.	be nice to your recruiter, which can be your biggest advocate
-	4.	during the day, do four or five interviews, with two different teams
-	5.	meet with your interviewers in their office
-	6.	depending on the team, interviewers may or may not share feedback on you with the rest of the interview loop
-	7.	when complete, you might speak with a hiring manager(as app or as appropriate), if so, a great sign
-	8.	it is now to the hiring manager's decision
-	9.	might get a decision that day, or it might be a week, after one week of no word from HR, send a freindly email asking for a status update
-	10.	if recruiter is not evry respnsive, because she's busy, not because you are silently rejected
+	Microsoft Interview:{
+		want passionate people about technology and smart people
+		write code on the board
+		1.	show up at Microsoft and fill out initial paper work
+		2.	short interview and a sample question, prep you
+		3.	be nice to your recruiter, which can be your biggest advocate
+		4.	during the day, do four or five interviews, with two different teams
+		5.	meet with your interviewers in their office
+		6.	depending on the team, interviewers may or may not share feedback on you with the rest of the interview loop
+		7.	when complete, you might speak with a hiring manager(as app or as appropriate), if so, a great sign
+		8.	it is now to the hiring manager's decision
+		9.	might get a decision that day, or it might be a week, after one week of no word from HR, send a freindly email asking for a status update
+		10.	if recruiter is not evry respnsive, because she's busy, not because you are silently rejected
 
-Definitely Prepare:
-	Why do you want to work for Microsoft?
-	want to see you are passionate sbout technology
-	"I've been using Microsof sofware as long as I can remember, and I'm really impressed at how Microsof
-	manages to create a product that is universally excellent. For example, I've been usingVisual Studio recently
-	to learn game programming, and its APls are excellent:" Note how this shows a passion fr technology!
+	Definitely Prepare:
+		Why do you want to work for Microsoft?
+		want to see you are passionate sbout technology
+		"I've been using Microsof sofware as long as I can remember, and I'm really impressed at how Microsof
+		manages to create a product that is universally excellent. For example, I've been usingVisual Studio recently
+		to learn game programming, and its APls are excellent:" Note how this shows a passion fr technology!
 
-What is Unique:
-	you will only reach the hiring manager if you have done well, so if you do, that is a great sign
-	Microsoft tends to give teams more individual control, and the product set is diverse
-	experiences can vary substantially across Microssoft since different teams look for different things
+	What is Unique:
+		you will only reach the hiring manager if you have done well, so if you do, that is a great sign
+		Microsoft tends to give teams more individual control, and the product set is diverse
+		experiences can vary substantially across Microssoft since different teams look for different things
 
-The Amazon Interview:
+	}
+
+	Amazon Interview: {
+		1.	begin with a phone screen in which candidate interview with a specific team
+		2.	write code via shared document editors
+		3.	ask a broad set of questions to explore what areas of technology you are familiar with
+		4.	fly to Seattle or whichever office, for four or five interviews
+		5.	code on whiteboard, some interviewers will stress other skills
+		6.	interviewer are assigned a specific area to probe, cannot see the other feedback until have submitted their own, they are discouraged from discussing it until the hiring meeting
+		7.	one bar raiser interviewer, charged with keeping the interview bar high
+		8.	if one interview seems significantly harder and different, that is most likely the bar raiser
+		9.	that person is both significant experience with interviews and veto power in the hiring decision
+		10.	remember: seem to be struggling more in this interview does not mean you are actually doing worse, relative to other candidates
+		11.	interviewers will be the people making the hiring decision
+		12.	occasionally there are some delays, have not heard from Amazon within a week, a freindly email recommended
+
+	Definitely Prepare:
+		cares a lot about scale, prepare fro scalability questions,
+		do not need a back-ground in distributed systems to answer these questions
+		lots of questions about oject-oriented design
+
+	What is Unique:
+		Bar raiser from a different team to keep the bar high, need to impress both this person
+		and the hiring manager
+	}
+		
+
+	Google Interview: {
+		not terribly different from Microsoft or Amazon
+		1.	first phone screen, expect tough technical questions, involve coding, sometimes via a shared document
+		2.	on-site interview, with four to six people, one of whom will be a lunch interviewer
+		3.	interviewer feedback is kept confidential from other interviewers, be assured that you enter each interview with blank slate
+		4.	lunch interviewer does not submit feedback, so can ask honest questions
+		5.	feedback is submitted to HC to make decision, feedback is typically broken down into four categories
+		6.	four categories: Analytical Ability, Coding, Experience, Communication
+		7.	HC does not include any of your interviewers, if does, purely random
+		8.	to extend an offer, HC wants to see at least one interviewer who is enthusiastic endorser
+		9.	phone screen performance is usually not a strong factor in the final decision
+		10.	return a decision take several weeks
+
+	Definitely Prepare:
+		scalable system, system design amd scalability
+		strong focus on analytical skills, regardless of experience, need very well prepared
+
+	What is Unique:
+		interviewers do not make the hiring decision, enter feedback which is passed to a hiring committee
+		hiring committee recommends a decision which can be-though rarely is-rejected by Google executives.
+	}
+
+	Apple Interview: {
+		passion for the position and the company is also very important
+		at least be familiar with the system
+		1.	a recruiter phone screen to get basic sense of your skills
+		2.	followed up by a series of technical phone screen with team members
+		3.	you will then have 6 - 8 interviews with members of the team which you are interviewing, as well as key people with whom your team works
+		4.	expect a mix of one on one and two on one interviews, code on a whiteboard
+		5. 	lunch is with your potential future manager and it is still interview
+		6.	discouraged from sharing feedback between interviewers
+		7.	if everyone still feels you are a viable candidate, you will have an interview with the director and the VP of the organization to which you are applying
+		8.	this decision happens behind scenes, if do not pass, will be escorted out of building
+		9.	If you made it to the director and VP interviews, all of your interviewers will gather in a conference room to give an of cial thumbs up or thumbs down.
+		10.	feel free to ping your recruiter for updates
+
+	Definitely Prepare:
+		if you know what team you are interviewing with, read up on that product
+		what do you like about it?
+		what would you improve?
+		offer specific recommendations can show your passion for the job
+
+	What is Unique:
+		Apple employees are huge Apple fans. You should show this same passion in your interview.
+	}
+
+
+	Facebook Interview: {
+		1.	generally do one or two phone screens, technical and will involve coding, usually an online document editor.
+		2.	might be asked to do a homework assignment that will include a mix of coding and algorithms. Pay attention to your coding style here.
+		3.	on-site interview, primarily with other software engineers, hiring managers are also involved
+		4.	Each interviewer is given a "role" during the on-site interviews,
+			Behavioral ("Jedi"):  assesses your ability to be successful in Facebook environment.
+			Coding and Algorithms ("Ninja"); questions are designed to be challenging
+			Design/Architecture ("Pirate"): For a backend software engineer, you might be asked system design questions.
+		5.	You can typically expect two "ninja" interviews and one "jedi" interview. 
+		6.	interviewers submit written  edback, prior to discussing your performance with each other
+		7.	your interviewing team and a hiring manager get together to collaborateona naldecision. andsubmita nalhirerecommendation to the hiring committee.
+
+	Definitely Prepare:
+		wants developers with an entrepreneurial spirit, you should show that you love to build stuff fast.
+		hack together an elegant and scalable solution using any language of choice.
+		Knowing PHP is not especially important
+
+	What is Unique:
+		Facebook interviews developers for the company "in general" not for a specific team. 
+		If you are hired, you will go through a six-week "bootcamp" which will help ramp you up in the massive code base. 
+		You will get mentorship from senior devs, learn best practices
+		ultimately, get a greater flexibility in choosing a project than if you were assigned to a project in your interview.
+	}
+
+	Palantir Interview: {
+		1.	Palantir interviews for a specific team
+		2.	Occasionally, your application might be re-routed to another team where there is a better fit.
+		3.	starts with two phone interviews, about 30 to 45 minutes and will be primarily technical.
+		4.	Expect to cover a bit about your prior experience, with a heavy focus on algorithm questions.
+		5.	be sent a HackerRank coding assessment, will evaluate your ability to write optimal algorithms and correct code
+		6.	Less experienced candidates, such as those in college, are particularly likely to get such a test.
+		7.	successful candidates are invited to campus and will interview with up to five people.
+		8.	cover your prior experience, relevant domain knowledge, data structures and algorithms, and design.
+		9.	get a demo of Palantir products. Ask good questions and demonstrate your passion for the company.
+		10.	the interviewers meet to discuss your feedback with the hiring manager
+
+	Definitely Prepare:
+		harder questions, more challenging questions
+		Brush up on system design too if you are interviewing for a backend role
+
+	What is Unique:
+		you will be at your computer and can look up material as needed, do not walk into this unprepared. 
+		The questions can be extremely challenging and the efficiency of your algorithm will be evaluated.
+	}	
+}
+
+
+
+
+Special Situations: {
+
+}
+
+
+
+
+
+
+
+
+Before the Interview: {
+	Writing a Great Resume: {
+		Programming Languages and Software:
+			Software: 
+				1.	Microsoft Office can almost always be cut
+				2.	Visual Studio and Eclipse, top tech companies will not even care about that
+				3.	it will not hurt to list all, just takes up valuable space
+
+			Languages:
+				1.	List everything you have ever worked with is dangerous
+				2.	list most of the Languages you have used, but added your experience level
+					Languages: Java (expert}, C++ (proficient), JavaScript (prior experience).
+		Projects:
+			The projects should include your 2 - 4 most significant projects. 
+				1.	what the project is
+				2.	which Languages or technology it employed
+				3.	details: not required, only include them if they make you look better
+						whether individual or team project
+						whether completed for a course or independtly
+					independt projects are preferred over course projects
+			do not add too many projects
+			You could build a mobile app, a web app, or almost anything. 
+			The most important thing is that you are building something.
+		Appropriate Resume Length
+			strongly advised to keep a resume to one page if you have less than ten years of experience.
+			Shorter resumes are often more impressive.
+	}
+
+	Getting the Right Experience: {
+		For current students:
+			1.	Take the Big Project Classes
+			2.	Get an Internship
+			3.	Start Something
+		Professionals:
+			1.	Shift Work Responsibilities More Towards Coding
+			2.	Use Your Nights and Weekends, a great way to get experience with new technologies
+			3.	think in advance about where you want your career to go
+	}
+
+	Preparation Map
+}
+
+
+Behavioral Questions: {
 	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
