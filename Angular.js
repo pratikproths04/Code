5db965df088b0c6ecec1882dd00ad72f364309c6
Zhiyi Chen <zhiyi.chen83@gmail.com>
2018-08-20 00:01:27 -0700

    commit

---
+++ Angular.js
@@ -0,0 +1,66 @@
+//global name space
+//create container to advoid pollution
+//using an object
+var stevesApp = {}
+
+stevesApp.person = 'Steve';
+stevesApp.logPerson = function() {
+	console.log(stevesApp.person);
+}
+
+
+//Angular.js Modules, Apps, Controllers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
