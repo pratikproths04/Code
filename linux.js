80011025191bd59b0e092c34e3feda55d7a2d906
Thebatzy1 <zhiyi.chen83@gmail.com>
2018-08-20 21:39:00 -0700

    commit

---
+++ linux.js
@@ -0,0 +1,25 @@
+linux scripts
+
+vagrant status: check if box is running
+
+connect ssh, connect linux: vagrant ssh
+
+vagrant account/root account password: vagrant
+start new vagrant configuration: vagrant reload
+
+
+virtum machine:
+
+	exit
+
+	vagrant destroy
+
+	vagrant up test2
+
+
+ cat /vagrant/Vagrantfile
+ ls /vagrant
+
+vagrant halt
+
+
