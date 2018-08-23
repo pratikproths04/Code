01ef53da2d1b3a5f6144e98672fcc2c8dce31bb7
Zhiyi Chen <zhiyi.chen83@gmail.com>
2018-08-22 18:08:07 -0700

    commit

--- linux.js
+++ linux.js
@@ -23,3 +23,87 @@ virtum machine:
 vagrant halt
 
 
+
+linux command:
+
+ls -l
+	display all file number in the directory
+
+vim filename.sh
+	create files and open, filename is not very important
+
+cat filename
+	open the file and display
+
+chmod 755 filename
+	add excute permission to all users
+	r = 4
+	w = 2
+	x = 1
+
+./filename
+	excute the file
+	. this directory
+	/ directory seperator
+
+mv filename filename2
+	change filename
+
+touch filename
+	create empty file
+
+type instruction
+	if shellbuiltin
+
+
+
+
+
+	
+
+
+
+edit .sh file:
+
+i
+	insert then you can type
+
+esc
+	finish editing
+
+:wq
+	save the file
+
+
+
+
+
+linux script:
+
+# = Sharp
+//comment
+
+! = Bang
+
+#! = Shebang, 
+//it starts with it, it defines the type of commands
+
+echo
+	print on the screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
